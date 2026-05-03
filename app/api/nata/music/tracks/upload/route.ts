85b5a80dea0273d86dca8db6752cfd4017b00050
zlomke76-del <zlomke76@gmail.com>
2026-05-02 20:26:45 -0500

    Update route.ts

--- app/api/nata/music/tracks/upload/route.ts
+++ app/api/nata/music/tracks/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { supabaseAdmin } from "../../../../../lib/supabaseAdmin";
+import { supabaseAdmin } from "../../../../../../lib/supabaseAdmin";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
